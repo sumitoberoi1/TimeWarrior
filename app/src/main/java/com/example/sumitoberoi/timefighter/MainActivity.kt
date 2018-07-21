c9e3a3d76ee4e612431bf7aa9d642ec9ebd1d6e6
Sumit Oberoi <sumit.oberoi1@gmail.com>
2018-07-22 02:32:33 +0530

    Event added to button

--- app/src/main/java/com/example/sumitoberoi/timefighter/MainActivity.kt
+++ app/src/main/java/com/example/sumitoberoi/timefighter/MainActivity.kt
@@ -10,14 +10,25 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var tapmeButton:Button
     internal lateinit var gameScoreTextView:TextView
     internal lateinit var timeLeftTextView:TextView
+    internal var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configUI()
+    }
+
+    private fun configUI() {
         tapmeButton = findViewById<Button>(R.id.tap_me_button)
         gameScoreTextView = findViewById<TextView>(R.id.game_score_text_view)
         timeLeftTextView = findViewById<TextView>(R.id.time_left_text_view)
-        tapmeButton.setOnClickListener {
-
+        tapmeButton.setOnClickListener { view ->
+            incrementScore()
         }
+    }
+
+    private fun incrementScore() {
+        score += 1
+        var newScoreString = getString(R.string.your_score,score.toString())
+        gameScoreTextView.text = newScoreString
     }
 }
