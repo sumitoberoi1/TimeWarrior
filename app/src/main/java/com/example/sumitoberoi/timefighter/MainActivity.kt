3e49f722d33cd709dc247b8e3b7c71944f253dd4
Sumit Oberoi <sumit.oberoi1@gmail.com>
2018-07-22 02:16:07 +0530

    Property added to activity

--- app/src/main/java/com/example/sumitoberoi/timefighter/MainActivity.kt
+++ app/src/main/java/com/example/sumitoberoi/timefighter/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.sumitoberoi.timefighter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    internal lateinit var tapmeButton:Button
+    internal lateinit var gameScoreTextView:TextView
+    internal lateinit var timeLeftTextView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tapmeButton = findViewById<Button>(R.id.tap_me_button)
+        gameScoreTextView = findViewById<TextView>(R.id.game_score_text_view)
+        timeLeftTextView = findViewById<TextView>(R.id.time_left_text_view)
+        tapmeButton.setOnClickListener {
+
+        }
     }
 }
